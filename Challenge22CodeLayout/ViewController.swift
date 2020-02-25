6732580f5c93e8f4d20c37167259b009c11a4f48
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-25 11:42:07 -0500

    finished challenge 2.2 autoresizing mask without storyboard

--- Challenge22CodeLayout/ViewController.swift
+++ Challenge22CodeLayout/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var largePadding: CGFloat = 50
+    private var smallPadding: CGFloat = 25
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillLayoutSubviews() {
+        if greenView.superview == nil {
+            view.addSubview(greenView)
+            let width = view.bounds.width - 2 * largePadding
+            let height = view.bounds.height - 2 * largePadding
+            greenView.frame = CGRect(x: largePadding, y: largePadding, width: width, height: height)
+        }
+        
+        if redView.superview == nil {
+            greenView.addSubview(redView)
+            let width = greenView.frame.width - 2 * smallPadding
+            let redHeight: CGFloat = 100
+            let centerY = greenView.bounds.height / 2 - redHeight / 2
+            redView.frame = CGRect(x: smallPadding, y: centerY, width: width, height: 100)
+        }
+    }
+    
+    private let redView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.autoresizingMask = [.flexibleWidth, .flexibleTopMargin, .flexibleBottomMargin]
+        return view
+    }()
 
-
+    private let greenView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return view
+    }()
 }
-
